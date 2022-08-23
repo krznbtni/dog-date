db43d0352891f96aacf3380c90c51f9d50985103
Karzan Botani <karzan@botani.nu>
2022-08-23 20:11:22 +0200

    x

--- src/views/MapPage.vue
+++ src/views/MapPage.vue
@@ -16,6 +16,13 @@
         <capacitor-google-map id="map"></capacitor-google-map>
       </div>
 
+      <input
+      id="pac-input"
+      class="controls"
+      type="text"
+      placeholder="Search Box"
+    />
+
     </ion-content>
   </ion-page>
 </template>
@@ -42,8 +49,92 @@ const createMap = async () => {
     },
   });
 
-  void addMarkers();
+  const input = document.getElementById("pac-input") as HTMLInputElement;
+  // @ts-expecterror
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef
+  const searchBox = new google.maps.places.SearchBox(input);
+
+  let markers: Marker[] = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      // map.removeMarker()
+      // marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon as string,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef
+        size: new google.maps.Size(71, 71),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef
+        origin: new google.maps.Point(0, 0),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef
+        anchor: new google.maps.Point(17, 34),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    // map.fitBounds(bounds);
+  });
 };
+
+const addCurrentPosMarker = async () => {
+  return Geolocation.requestPermissions().then(async permission => {
+    console.log({permission})
+    const coordinates = await Geolocation.getCurrentPosition();
+    const marker: Marker = {
+      title: 'Current Pos',
+      coordinate: {
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude,
+      }
+    };
+
+    const addMarkerRes = await map.addMarker(marker);
+    console.log({addMarkerRes});
+    map.setOnMarkerClickListener(async(marker) => {
+    console.log(marker);
+  })
+  });
+}
 
 const addMarkers = async() => {
   const markers: Marker[] = [
@@ -81,5 +172,20 @@ capacitor-google-map {
 .map-container {
   height: 50%;
   width: 100%;
+}
+
+#pac-input {
+  background-color: #fff;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 400px;
+}
+
+#pac-input:focus {
+  border-color: #4d90fe;
 }
 </style>
