58c0ce51d003f4d4b7a6dc436ced5301698af6a7
Karzan Botani <karzan@botani.nu>
2022-08-24 18:15:30 +0200

    Refactor TabsPage to script setup

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -22,20 +22,7 @@
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
-
-export default defineComponent({
-  name: 'TabsPage',
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
-  setup() {
-    return {
-      ellipse, 
-      square, 
-      triangle,
-    }
-  }
-});
 </script>
